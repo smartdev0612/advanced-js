ea7d21d08569e4d68d583dcde5419ef2b969a006
smartdev0612 <danieldev0612@gmail.com>
2023-04-26 20:41:53 -0700

    advanced loop

---
+++ loop.js
@@ -0,0 +1,28 @@
+const basket = ['apples', 'oranges', 'grapes']
+const detailedBasket = {
+  apples: 5,
+  oranges: 10,
+  grapes: 1000,
+}
+
+// 1
+for (let i = 0; i < basket.length; i++) {
+  console.log(basket[i])
+}
+
+// 2
+basket.forEach((item) => {
+  console.log(item)
+})
+
+// for of
+// Iterating - arrays, strings
+for (item of basket) {
+  console.log(item)
+}
+
+// for in - properties
+// enumerating - objects
+for (item in detailedBasket) {
+  console.log(item)
+}

27825f29ba89890fd52089e242f1596c5886d69f
smartdev0612 <danieldev0612@gmail.com>
2023-04-26 20:14:53 -0700

    adding ES10 features

--- exercise.js
+++ exercise.js
@@ -1,4 +1,23 @@
-// ES10
+// ES10 - flat
 const array3 = [1, 2, [3, 4, [5]]]
 const flated_array = array3.flat(2)
-console.log(flated_array)
+// console.log(flated_array)
+
+const entries = ['bob', 'sally', , , , , 'cindy']
+// console.log(entries.flat())
+
+const array3Chaos = array3.flatMap((creature) => creature + '1')
+// console.log(array3Chaos)
+
+const userEmail = '   hell@mail.com'
+const userEmail1 = 'hello@gmail.com       '
+console.log(userEmail.trimStart())
+console.log(userEmail1.trimEnd())
+
+userProfiles = [
+  ['commanderTom', 23],
+  ['derekZlander', 40],
+  ['Gerad', 35],
+]
+const obj = Object.fromEntries(userProfiles)
+console.log(Object.entries(obj))

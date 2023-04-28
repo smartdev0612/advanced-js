c52382223ca6c8ea3cbfb15241e3c15fa5715ddf
smartdev0612 <danieldev0612@gmail.com>
2023-04-27 20:17:42 -0700

    replaceAll for ES2021

---
+++ es2021.js
@@ -0,0 +1,6 @@
+// ES2021
+// replaceAll
+const str =
+  'Every action you take is a vote for the type of person you wish to become.'
+const str1 = str.replaceAll('vote', 'demand')
+console.log(str1)

cd4fa49984ab2373dcc083b9c934be8bf26bfe44
smartdev0612 <danieldev0612@gmail.com>
2023-04-27 19:02:19 -0700

    bigint in ES2020

---
+++ es2020.js
@@ -0,0 +1,9 @@
+// Bigint
+console.log(1n + 2n)
+
+console.log(Number.MAX_SAFE_INTEGER)
+
+console.log(9007199254740991 + 10) // => Wrong!
+console.log(9007199254740991n + 10n)
+
+console.log(typeof 3n)
